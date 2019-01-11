7932749e93673f333f8aafa6445f7d3093815012
zhangli <37255537@qq.com>
2019-01-11 18:07:07 +0800

    增加target指定

--- cppmake.py
+++ cppmake.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def addtoArr(arr, templ, s):
-    dirs = s.split(',')
-    for d in dirs:
-        arr.append(templ % d.strip())
-
-def replaceTempl(templ, flag, arr):
-    return templ.replace('{%s}' % flag, ' '.join(arr))
+import os
+import sys
 
 TEMPL = '''# Makefile
-#
-# usage :
-#     make
-#     make BUILD=release
-#	  make clean
-#
 
 LIBPATH = -L./
 INC = {inc}
 LIBS = -lpthread
 TARGET_BIN = "{out}"
+TARGET_PATH = "{__oup}"
 COMMFLAG = -pipe -Wall -Wno-trigraphs -Werror
-CXX = ccache g++ -std=gnu++1y
-CC = ccache gcc -std=c11
+CXX = g++ -std=gnu++1y
+CC = gcc -std=c11
 
 CXXFLAGS = $(COMMFLAG) -g3 -ggdb3 -O0 -D_DEBUG
 OBJPATH = {tmp}/
@@ -38,12 +28,11 @@ clean :
 
 
 AllDirs := {src}
-$(warning $(AllDirs))
 SRCS_LIB := $(foreach n,$(AllDirs) , $(wildcard $(n)/*.cpp))
 SRCS_LIB_C := $(foreach n,$(AllDirs) , $(wildcard $(n)/*.c))
 SRCS_LIB_CC := $(foreach n,$(AllDirs) , $(wildcard $(n)/*.cc))
 OBJS_BIN = $(SRCS_LIB:%.cpp=$(OBJPATH)/%.o) $(SRCS_LIB_C:%.c=$(OBJPATH)/%.o) $(SRCS_LIB_CC:%.cc=$(OBJPATH)/%.o)
-$(warning $(OBJS_BIN))
+#$(warning $(OBJS_BIN))
 
 ifneq "$(MAKECMDGOALS)" "clean"
 -include $(SRCS_LIB:%.cpp=$(OBJPATH)/%.d)
@@ -52,6 +41,7 @@ ifneq "$(MAKECMDGOALS)" "clean"
 endif
 
 $(TARGET_BIN) : $(OBJS_BIN)
+	@mkdir -p $(TARGET_PATH)
 	$(CXX) $(CXXFLAGS) -o $@ $^ $(LIBS)
 
 $(OBJPATH)/%.o : %.cpp
@@ -86,25 +76,50 @@ $(OBJPATH)/%.d : %.cc
 
 '''
 
+dirtempl = r'''$(shell ls -R %s|grep '^\./.*:$$'|awk '{gsub(":","");print}')'''
+inctempl = '''-I%s'''
+deftempl = '%s'
+
 src = []
 inc = []
 tmp = ['./build/temp']
 out = ['a.out']
+outpath = ['./']
 
-dirtempl = r'''$(shell ls -R %s | grep '^\./.*:$$' | awk '{gsub(":","");print}')'''
-inctempl = '''-I%s'''
-deftempl = '%s'
+import_files = []
 
 templ_table = (
-    ('src', dirtempl, src),
-    ('inc', inctempl, inc),
-    ('tmp', deftempl, tmp),
-    ('out', deftempl, out),
+    ('src', dirtempl, src, 0),
+    ('inc', inctempl, inc, 0),
+    ('tmp', deftempl, tmp, 1),
+    ('out', deftempl, out, 1),
+    ('add', deftempl, import_files, 0),
+    ('__oup', deftempl, outpath, 1),
 )
+
+proced_files = set()
 
 templ_map = {}
 for tr in templ_table: 
     templ_map[tr[0]+':'] = (len(tr[0])+1, tr[1], tr[2])
+
+
+def getfilename():
+    if len(sys.argv) == 1:
+        return "cppmake.txt"
+    elif len(sys.argv) == 2:
+        return sys.argv[1]
+    else:
+        quit()
+
+def addtoArr(arr, templ, s):
+    dirs = s.split(',')
+    for d in dirs:
+        arr.append(templ % d.strip())
+
+
+def replaceTempl(templ, flag, arr):
+    return templ.replace('{%s}' % flag, ' '.join(arr))
 
 
 def getparam(line):
@@ -115,30 +130,60 @@ def getparam(line):
         if key == k:
             addtoArr(arr, tmpl, line[kl:])
 
-import sys, os
+def proc_cppmake(fn):
+    if fn in proced_files:
+        return
 
-if len(sys.argv) == 1:
-    arg = "cppmake.txt"
-else:
-    arg = sys.argv[1]
+    fd = open(fn, "rb")
+    if not fd:
+        print("no inputfile")
+        quit()
 
-fd = open(arg, "rb")
-if not fd:
-    print("no inputfile")
-    quit()
+    txt = fd.read()
+    fd.close()
+    cmdlines = txt.split('\n')
 
-txt = fd.read()
-fd.close()
-cmdlines = txt.split('\n')
+    for line in cmdlines:
+        line = line.strip()
+        getparam(line)
 
-for line in cmdlines:
-    line = line.strip()
-    getparam(line)
+    proced_files.add(fn)
 
-fd = open("Makefile", "wb+")
-result = replaceTempl(TEMPL, 'src', src)
-result = replaceTempl(result, 'inc', inc)
-result = replaceTempl(result, 'tmp', tmp[-1:])
-result = replaceTempl(result, 'out', out[-1:])
-fd.write(result)
-fd.close()
+    proc_import()
+
+def proc_import():
+    global import_files
+    temp = import_files
+    import_files = []
+    
+    while True:
+        for fn in temp:
+            #print('proc import %s' % fn)
+            proc_cppmake(fn)
+        if not import_files:
+            break
+
+def proc_something():
+    arr = out[-1:][0]
+    oup = arr.split('/')
+    oup.pop()
+    outpath.append('/'.join(oup))
+
+
+def export_Makefile():
+    fd = open("Makefile", "wb+")
+    result = TEMPL
+    for tr in templ_table:
+        if tr[3] == 0:
+            result = replaceTempl(result, tr[0], tr[2])
+        else:
+            result = replaceTempl(result, tr[0], tr[2][-1:])
+
+    fd.write(result)
+    fd.close()
+
+if __name__ == "__main__":
+    fn = getfilename()
+    proc_cppmake(fn)
+    proc_something()
+    export_Makefile()
